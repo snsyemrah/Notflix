75bd5c5234ab0b35be56c12a8c97adb274106f94
HDURSUN <halil.dursun@capgemini.com>
2023-08-09 13:55:43 +0200

    new functionalities added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PhotographsComponent} from 'src/app/photographs/photographs.component';
 import { LoginComponent} from 'src/app/login/login.component';
-import { AppComponent } from 'src/app/app.component';
+import { HomeComponent } from 'src/app/home/home.component';
 const routes: Routes = [
-      { path: '', component: AppComponent},
+      { path: '', component: HomeComponent},
       { path: 'login', component: LoginComponent },
-      { path: 'movies:id', component: AppComponent}
+//       { path: 'movies:id', component: AppComponent}
 ];
 // const routes: Routes = [];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
-import { TestcomponentComponent } from './testcomponent/testcomponent.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PhotographsComponent } from './photographs/photographs.component';
 import { BannerComponent } from './banner/banner.component';
 import { LoginComponent } from './login/login.component';
+import { FilmstripComponent } from './filmstrip/filmstrip.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestcomponentComponent,
     PhotographsComponent,
     BannerComponent,
-    LoginComponent
+    LoginComponent,
+    FilmstripComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
